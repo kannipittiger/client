0c8e00a575d32f0ebdc316168d793a2b98132b77
b6430300463 <140489891+b6430300463@users.noreply.github.com>
2023-10-21 16:14:57 +0000

    update color register&login

--- src/welcome/login.js
+++ src/welcome/login.js
@@ -2,15 +2,15 @@ import './style/loginStyle.css'
 export default function Login () {
     return(
         <div className="login">
-            <div>
+            <div className="border-login">
                 <h2>
                     Log in to Moodify
                 </h2>
                 <h5>
                     Email address<br/>
-                    <input className="input" type="text" name="email" required></input><br/>
+                    <input className="input-login" type="text" name="email" required></input><br/>
                     Password<br/>
-                    <input className="input" type="password" name="password" required></input>
+                    <input className="input-login" type="password" name="password" required></input>
                 </h5>
                 <button className="login-btn" id="font-btn">Log in</button>
                 <br/>
--- src/welcome/register.js
+++ src/welcome/register.js
@@ -2,21 +2,19 @@ import './style/registerStyle.css'
 export default function Register () {
     return(
         <div className="register">
-            <div>
+            <div className="border-register">
                 <h2>
                     Sign up to Start<br/>Moodify
                 </h2>
                 <h5>
                     Email address<br/>
-                    <input className="input" type="text" name="email" required></input>
-                </h5>
-                <h5>
+                    <input className="input-register" type="text" name="email" required></input>
+                    <br/>
                     Username<br/>
-                    <input className="input" type="text" name="username" required></input>
-                </h5>
-                <h5>
+                    <input className="input-register" type="text" name="username" required></input>
+                    <br/>
                     Password<br/>
-                    <input className="input" type="password" name="password" required></input>
+                    <input className="input-register" type="password" name="password" required></input>
                 </h5>
                 <button className="register-btn" id="font-btn">Sign up</button>
                 </div>
