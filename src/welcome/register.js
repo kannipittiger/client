ceda5ec5a5e61c4cd3949174bfe5735ae7a51e75
b6430300463 <nanthapong.l@ku.th>
2023-10-22 23:53:55 +0700

    update register form

--- src/welcome/register.js
+++ src/welcome/register.js
@@ -1,6 +1,7 @@
 import './style/registerStyle.css'
-import { Link } from 'react-router-dom'
+
 const Register = () => {
+
     return(
         <div className="register">
             <div className="border-register">
@@ -8,18 +9,23 @@ const Register = () => {
                     Sign up to Start<br/>Moodify
                 </h2>
                 <h5>
-                    Email address<br/>
-                    <input className="input-register" type="text" name="email" required></input>
-                    <br/>
-                    Username<br/>
-                    <input className="input-register" type="text" name="username" required></input>
-                    <br/>
-                    Password<br/>
-                    <input className="input-register" type="password" name="password" required></input>
+                    <form method="post" action="register_db.php">
+                        <label for="email">Email address</label>
+                        <br/>
+                        <input className="input-register" type="email" name="email" placeholder='Enter email' required></input>
+                        <br/>
+                        <label for="username">Username</label>
+                        <br/>
+                        <input className="input-register" type="text" name="username" placeholder='Enter username' required></input>
+                        <br/>
+                        <label for="password">Password</label>
+                        <br/>
+                        <input className="input-register" type="password" name="password" placeholder='Enter password' required></input>
+                        <br/><br/>
+                        <button name="reg_user" type="submit" className="register-btn font-btn">Sign up</button>
+                    </form>
                 </h5>
-                <nav>
-                <Link to="/"><button className="register-btn" id="font-btn">Sign up</button></Link>
-                </nav>
+                
                 </div>
         </div>
     );
