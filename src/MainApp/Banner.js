379e605f37ebc1dee21e627d4606075193c515da
FrameFrame643 <werty.Rong@gmail.com>
2023-10-22 14:45:42 +0700

    addbanner

---
+++ src/MainApp/Banner.js
@@ -0,0 +1,8 @@
+export default function banner() {
+    return(
+        <div stlye={{backgoundImage:`url("https://miro.medium.com/v2/resize:fit:1400/1*AkvNtnJyqThYznytQs9VKQ.jpeg")`}}>
+
+        </div>
+    )
+}
+    
