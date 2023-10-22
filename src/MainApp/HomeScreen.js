b7ff23600f311d058744302b7c276a01691ebfce
Kannipit Pornpayap <kannipit.p@ku.th>
2023-10-22 20:57:10 +0700

    layout

--- src/MainApp/HomeScreen.js
+++ src/MainApp/HomeScreen.js
@@ -1,17 +1,25 @@
-import './Mainstyle/HomeStyle.css'
+import './Mainstyle/HomeStyle.css';
+import React from 'react'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import { AiFillHome } from "react-icons/ai";
+import {BsFillSearchHeartFill,BsFillCollectionFill,BsMusicNoteList} from "react-icons/bs";
+
 
 export const Home = () => {
     return(
         <div className='allbox'>
             <div className='leftbox'>
                 <div className='upbox'>
-                    <button>home</button>
-                    <button>search</button>
+                    <button className='btn1'><AiFillHome /> home</button>
+                    <button className='btn2'><BsFillSearchHeartFill /> search</button>
                 </div>
-                <div className='downbox'></div>
+                <div className='downbox'>
+                    <div className='textys'><BsFillCollectionFill style={{paddingRight:20}}/>Your songs</div>
+                    <div></div>
+                </div>
             </div>
             <div className='rightbox'>
-
+                <div className='textrp'><BsMusicNoteList size={40}style={{paddingRight:20}}/>Recently played</div>
             </div>
         </div>
     )
