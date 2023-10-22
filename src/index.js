dcd4884130c885d0cc1e83b6ba32efb8acaae2b9
Kannipit Pornpayap <kannipit.p@ku.th>
2023-10-22 23:48:45 +0700

    mainscreen#2

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import Banner from './MainApp/Banner'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home />
+    <App />
   </React.StrictMode>
 );
 
