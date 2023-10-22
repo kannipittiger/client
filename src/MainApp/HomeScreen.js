22f7451389ceedb626de8003ef07ecfad79be552
Kannipit Pornpayap <kannipit.p@ku.th>
2023-10-22 17:50:45 +0700

    add home

---
+++ src/MainApp/HomeScreen.js
@@ -0,0 +1,20 @@
+import './Mainstyle/HomeStyle.css'
+
+export const Home = () => {
+    return(
+        <div className='allbox'>
+            <div className='leftbox'>
+                <div className='upbox'>
+                    <button>home</button>
+                    <button>search</button>
+                </div>
+                <div className='downbox'></div>
+            </div>
+            <div className='rightbox'>
+
+            </div>
+        </div>
+    )
+}
+
+export default Home;
