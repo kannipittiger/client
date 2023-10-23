9e738cab3b5c8d24a86cdb5d0fa478210c9f44fb
FrameFrame <91085028+FrameFrame643@users.noreply.github.com>
2023-10-23 08:57:31 +0000

    Haveaudio

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ import tracks from "./Audio/tracks";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home />
+    <App />
   </React.StrictMode>
 );
 
