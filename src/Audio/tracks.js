a4304c0984c77552f46adab13e096ace367fbad5
FrameFrame <91085028+FrameFrame643@users.noreply.github.com>
2023-10-23 08:48:20 +0000

    HaveAudio

---
+++ src/Audio/tracks.js
@@ -0,0 +1,14 @@
+
+import cali from "./assets/Mac Miller - Self Care.mp3";
+
+
+// All of these artists are at https://pixabay.com/music/search/mood/laid%20back/
+export default [
+  {
+    title: "Cali",
+    artist: "Wataboi",
+    audioSrc: "https://audio.jukehost.co.uk/buM5vIGR3O63vrHHqSmcXaieadVRI3as",
+    image: '',
+    color: "#00aeb0"
+  }
+];
--- src/index.js
+++ src/index.js
@@ -8,11 +8,14 @@ import Welcome from './welcome/welcome'
 import Login from './welcome/login'
 import Register from './welcome/register'
 import Banner from './MainApp/Banner'
+import AudioPlayer from "./Audio/AudioPlay";
+import tracks from "./Audio/tracks";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home />
+    <App/>
+     {/* <AudioPlayer tracks={tracks} /> */}
   </React.StrictMode>
 );
 
