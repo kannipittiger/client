f3596454352e179e5035fb4eea8e002b072097a3
FrameFrame <91085028+FrameFrame643@users.noreply.github.com>
2023-10-23 14:01:02 +0000

    addsome

--- src/index.js
+++ src/index.js
@@ -15,8 +15,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    {/* <AudioPlayer tracks={tracks} /> */}
+    {/*<AudioPlayer tracks={tracks} />*/}
   </React.StrictMode>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
